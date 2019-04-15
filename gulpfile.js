0a6790f601ee766dc64017cec079d300e37202c0
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-04-15 16:55:45 +0200

    Added browsersync
    Added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,34 @@
 // Load the dependencies in variables
 var gulp = require('gulp'); 
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var SOURCEPATHS = {
+  sassSource: 'src/scss/*.scss'
+}
+
+var APPPATH = {
+  root: 'app',
+  css: 'app/css',
+  js: 'app/js'
+}
 
 // Create gulp task named Sass
 // Picks up sccs file from 'src/scss/app/scss' and converts it into css file on destination '/app/css'
 gulp.task('sass', function(){
-  return gulp.src('src/scss/app.scss')
-  .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-  .pipe(gulp.dest('app/css'));
+  return gulp.src(SOURCEPATHS.sassSource)
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest(APPPATH.css));
 })
 
+gulp.task('serv', ['sass'], function(){
+   browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+     server: {
+       baseDir: APPPATH.root
+     }
+   })
+});
+
 // Create gulp task named default and executes 'sass' task
-gulp.task('default', ['sass']);
+gulp.task('default', ['serv']);
