31a92651588c041639e39ae7635eb38588e6d200
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-04-15 17:27:15 +0200

    Added dependency autoprefixer
    Added dependency autoprefixer: Sets browser prefixers automatically

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@
 // Load the dependencies in variables
 var gulp = require('gulp'); 
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+var browsersync = require('browser-sync');
+var reload = browsersync.reload;
+var autoprefixer = require('gulp-autoprefixer');
 
 var SOURCEPATHS = {
   sassSource: 'src/scss/*.scss'
@@ -19,12 +20,16 @@ var APPPATH = {
 // Picks up sccs file from 'src/scss/app/scss' and converts it into css file on destination '/app/css'
 gulp.task('sass', function(){
   return gulp.src(SOURCEPATHS.sassSource)
+     // Adding browser prefixed back to 4 versions if necessary
+    .pipe(autoprefixer('last 4 versions'))
+    // convers scss to css file. See different outputstyles: https://web-design-weekly.com/2014/06/15/different-sass-output-styles/
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    // set file to destination
     .pipe(gulp.dest(APPPATH.css));
 })
 
 gulp.task('serv', ['sass'], function(){
-   browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+   browsersync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
      server: {
        baseDir: APPPATH.root
      }
