97aac7ab4d04cac568e72895a3c3554c0b789d1e
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-04-16 21:23:47 +0200

    Added Bootstrap Carousel to check if dependencies are ok
    Added Bootstrap Carousel to check if dependencies are ok

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,3 @@ global.jQuery = require('jquery');
 popper = require('popper.js');
 bootstrap = require('bootstrap');
 mustache = require('mustache');
-
-jQuery(document).ready(function() {
-    alert("hello");
-});
